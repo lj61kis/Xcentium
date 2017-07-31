74f3c6834e6406b0921ad7648408c514a750f920
Leslie Jasper <Leslie.Jasper@greyhound.com>
2017-07-31 16:46:55 -0500

    Add project files.

---
+++ Content/js/site.js
@@ -0,0 +1,31 @@
+﻿$(function () {
+
+    var image = $('.grid-image img');
+
+    image.draggable({
+        
+        revert: true,
+        zIndex: 1000,
+        cursor: "move"
+    }).droppable({
+        over: function(event, ui){
+            $(this).addClass("ui-droppable-hover");
+        },
+        out: function(event,ui){
+            $(this).removeClass("ui-droppable-hover");
+        },
+        drop: function (event, ui) {
+            var dropped = ui.draggable;
+            var droppedOn = $(this);
+            
+            var imageSwapped = droppedOn.attr("src");
+            var imageOriginal = dropped.attr("src");
+
+            dropped.attr("src", imageSwapped);
+            droppedOn.attr("src", imageOriginal);
+
+            $(this).removeClass("ui-droppable-hover");
+        }
+    });
+
+});
---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+﻿module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'content/sass/*.scss',
+                    cssDir: 'content/css',
+                    environment: 'production'
+                }
+            },
+            dev: {
+                options: {
+                    sassDir: 'content/sass',
+                    cssDir: 'content/css'
+                }
+            }
+        },
+
+        watch: {
+            sass: {
+                files: ['content/sass/*.scss'],
+                tasks: ['sass']
+            }
+        },
+
+        sass: {
+            dist: {
+                files: {
+                 'Content/css/style.css':'Content/sass/style.scss'
+                }
+            }
+        },
+
+    });
+
+    // Load the plugin that provides the "compass" task.
+    grunt.loadNpmTasks('grunt-contrib-compass');
+
+    // Load the plugin that provides the "watch" task.
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Load the plugin that provides the "sass" task.
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    // Default task(s).
+    grunt.registerTask('default', ['watch']);
+};
